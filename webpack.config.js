bec1eab745f143dd185bf6359d7c18344629b49b
dkegle <domenkeglevic@gmail.com>
2018-09-18 12:59:53 +0200

    Setup css

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './'
+            }
+          },
+          "css-loader"
+        ]
+      }
+     ]
+   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
+  ],
+  mode: 'development'
+};
